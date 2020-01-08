66b1340bd16819e643797aeaa39e76e6003eebdb
StoneT2000 <stoneztao@gmail.com>
2020-01-07 20:01:28 -0800

    Untested netgun code

--- src/bot1/NetGun.java
+++ src/bot1/NetGun.java
@@ -6,8 +6,23 @@ public class NetGun extends RobotPlayer {
     public static void run() throws GameActionException {
         RobotInfo[] nearbyEnemyRobots = rc.senseNearbyRobots(-1, enemyTeam);
 
+        RobotInfo closestBot = null;
+        int closestEnemyDist = 99999999;
         for (int i = nearbyEnemyRobots.length; --i >= 0; ) {
             RobotInfo info = nearbyEnemyRobots[i];
+            // TODO: Check if info.getLocation() vs info.location is more efficient?
+            int dist = rc.getLocation().distanceSquaredTo(info.getLocation());
+            if (dist < closestEnemyDist) {
+                closestEnemyDist = dist;
+                closestBot = info;
+            }
+        }
+        // if we found a closest bot
+        if (closestBot != null) {
+            if (rc.canShootUnit(closestBot.getID())) {
+                rc.shootUnit(closestBot.getID());
+                if (debug) rc.setIndicatorDot(closestBot.location, 255, 50,190);
+            }
         }
 
         /* SCOUTING CODE */
