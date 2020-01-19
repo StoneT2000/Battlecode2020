5b3442c25045fa84604d4bba061d01a41f5dec1e
StoneT2000 <stoneztao@gmail.com>
2020-01-19 08:24:36 -0800

    c

--- src/Chow6/RobotPlayer.java
+++ src/Chow6/RobotPlayer.java
@@ -21,7 +21,7 @@ public strictfp class RobotPlayer {
     static MapLocation enemyBaseLocation = null; // the enemy HQ location
 
     static int turnCount;
-    static final boolean debug = true;
+    static final boolean debug = false;
     static final int UNIQUEKEY = -1893287461;
     static Team enemyTeam; // enemy team enum
 
