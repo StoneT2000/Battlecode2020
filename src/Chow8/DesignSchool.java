9b71a426d7c77968e47a2cb28720430713f71351
StoneT2000 <stoneztao@gmail.com>
2020-01-22 22:30:14 -0800

    V8 lets go

---
+++ src/Chow8/DesignSchool.java
@@ -0,0 +1,106 @@
+package Chow8;
+
+import battlecode.common.*;
+
+public class DesignSchool extends RobotPlayer {
+    static Direction buildDir = Direction.NORTH;
+    static int landscapersBuilt = 0;
+    static int vaporatorsBuilt = 0;
+    static boolean needLandscaper = false;
+    static boolean dontBuild = false;
+    static boolean terraformingTime = false;
+    public static void run() throws GameActionException {
+        RobotInfo[] nearbyEnemyRobots = rc.senseNearbyRobots(-1, enemyTeam);
+
+        for (int i = nearbyEnemyRobots.length; --i >= 0; ) {
+            RobotInfo info = nearbyEnemyRobots[i];
+        }
+        Transaction[] lastRoundsBlocks = rc.getBlock(rc.getRoundNum() - 1);
+        boolean willBuild = false;
+        if (rc.getTeamSoup() >= RobotType.LANDSCAPER.cost && landscapersBuilt < 2) {
+            willBuild = true;
+        }
+        checkMessages(lastRoundsBlocks);
+        if (needLandscaper) {
+            willBuild = true;
+            needLandscaper = false;
+            if (debug) System.out.println("Building landscaper as asked by HQ ");
+        }
+        if (vaporatorsBuilt * 2 + 2 > landscapersBuilt && rc.getTeamSoup() >= RobotType.LANDSCAPER.cost + 250) {
+            willBuild = true;
+            if (debug) System.out.println("Building landscaper matching vaporators");
+        }
+        if (rc.getTeamSoup() > 1000) {
+            willBuild = true;
+        }
+        if (dontBuild) {
+            willBuild = false;
+            dontBuild = false;
+        }
+
+        if (terraformingTime && rc.getTeamSoup() >= RobotType.LANDSCAPER.cost && rc.getLocation().distanceSquaredTo(HQLocation) <= 48) {
+            if (vaporatorsBuilt > 8) {
+                if (vaporatorsBuilt * 5+ 5 > landscapersBuilt) {
+                    willBuild = true;
+                }
+            }
+            else {
+                if (vaporatorsBuilt * 1 > landscapersBuilt) {
+                    willBuild = true;
+                }
+            }
+
+        }
+
+        if (debug) System.out.println("Trying to build: " + willBuild);
+        if (willBuild) {
+            // should rely on some signal
+            boolean builtUnit = false;
+            buildDir = rc.getLocation().directionTo(HQLocation);
+            for (int i = 9; --i >= 1; ) {
+                if (tryBuild(RobotType.LANDSCAPER, buildDir) && !isDigLocation(rc.adjacentLocation(buildDir))) {
+                    builtUnit = true;
+                    break;
+                } else {
+                    buildDir = buildDir.rotateRight();
+                }
+            }
+            if (builtUnit) {
+                landscapersBuilt++;
+            }
+            buildDir = buildDir.rotateRight();
+        }
+
+    }
+    static void checkMessages(Transaction[] transactions) throws GameActionException {
+        for (int i = transactions.length; --i >= 0;) {
+            int[] msg = transactions[i].getMessage();
+            decodeMsg(msg);
+            if (isOurMessage((msg))) {
+                // FIXME: Buggy, better way?
+                if ((msg[1] ^ NEED_LANDSCAPERS_FOR_DEFENCE) == 0) {
+                    int origSoup = msg[2];
+                    int soupSpent = origSoup - rc.getTeamSoup();
+                    // if soup spent / number of landscapers needed is greater than cost
+                    if (soupSpent / msg[3] < RobotType.LANDSCAPER.cost) {
+                        needLandscaper = true;
+                    }
+                }
+                else if ((msg[1] ^ BUILD_DRONE_NOW) == 0) {
+                    dontBuild = true;
+                }
+                else if (msg[1] == RobotType.VAPORATOR.ordinal()) {
+                    vaporatorsBuilt++;
+                }
+                else if ((msg[1] ^ NO_MORE_LANDSCAPERS_NEEDED) == 0) {
+                    terraformingTime = true;
+                }
+            }
+        }
+
+    }
+    public static void setup() throws GameActionException {
+        storeHQLocation();
+        announceSelfLocation(1);
+    }
+}
---
+++ src/Chow8/Refinery.java
@@ -0,0 +1,41 @@
+package Chow8;
+
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotInfo;
+
+public class Refinery extends RobotPlayer {
+    static boolean announcedSelf = false;
+    public static void run() throws GameActionException {
+
+        if (!announcedSelf && rc.getTeamSoup() >= 1) {
+            announceSelfLocation(1);
+            announcedSelf = true;
+        }
+
+        RobotInfo[] nearbyEnemyRobots = rc.senseNearbyRobots(-1, enemyTeam);
+
+        for (int i = nearbyEnemyRobots.length; --i >= 0; ) {
+            RobotInfo info = nearbyEnemyRobots[i];
+        }
+
+        /* BIG BFS LOOP ISH */
+        for (int i = 0; i < Constants.BFSDeltas24.length; i++) {
+            int[] deltas = Constants.BFSDeltas24[i];
+            MapLocation checkLoc = rc.getLocation().translate(deltas[0], deltas[1]);
+            // TODO: instead of canSenseLocation, maybe do the math and choose the right BFS deltas to iterate over
+            if (rc.canSenseLocation(checkLoc)) {
+
+            }
+            else {
+                break;
+            }
+        }
+    }
+    public static void setup() throws GameActionException {
+        if (rc.getTeamSoup() >= 1) {
+            announceSelfLocation(1);
+            announcedSelf = true;
+        }
+    }
+}
