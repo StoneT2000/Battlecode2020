8f60790bcc5e8d296f2cafca09221ebeaf6d3698
StoneT2000 <stoneztao@gmail.com>
2020-01-27 21:07:39 -0800

    chgange

--- src/Chow10/RobotPlayer.java
+++ src/Chow10/RobotPlayer.java
@@ -28,7 +28,7 @@ public strictfp class RobotPlayer {
 
     static int turnCount;
     static final boolean debug = false;
-    static final int UNIQUEKEY = -999777234;
+    static final int UNIQUEKEY = -111777234;
     static Team enemyTeam; // enemy team enum
 
     static final int BASE_WALL_DIST = 1;
